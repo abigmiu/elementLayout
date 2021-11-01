e347dd5e1831cc6d3d4eab70985fd16102d9402f
abigmiu <1552487872@qq.com>
2021-11-01 10:29:39 +0800

    form组件

--- src/components/form.vue
+++ src/components/form.vue
@@ -1,81 +1,133 @@
 <template>
-  <div>
-    <el-form
-      ref="formRef"
-      :label-position="labelPosition"
-      :model="innerFormData"
-      :label-width="labelWidth"
-    >
-      <el-form-item
-        v-for="item in items"
-        v-show="
-          item.visiable === undefined ? true : item.visiable
-        "
-        :label="item.label"
-        :key="item.value"
-        :rules="
-          item.rules || [
-            {
-              required: item.required,
-              message: item.message
-            }
-          ]
-        "
-      >
-        <!-- 数字输入框 -->
-        <template v-if="item.type === number">
-          <el-input-number
-            v-model.number="innerFormData[item.value]"
-            controls-position="right"
-            :min="item.min"
-            :max="item.max"
-            :readonly="item.readonly"
-            :placeholder="item.placeholder"
-            @change="handleChange(item)"
-          ></el-input-number>
-        </template>
-        <!-- 输入框 -->
-        <template v-if="item.type === 'input'">
-          <el-input
-            v-model="innerFormData[item.value]"
-            :type="itme.tag"
-            :placeholder="item.placeholder"
-            :row="item.row"
-            :autosize="item.autosize"
-            :maxlength="item.maxlength"
-            :show-word-limit="item.maxlength"
-            :show-password="item.tag === 'password'"
-            @change="handleChange(item)"
-          >
-          </el-input>
-        </template>
-      </el-form-item>
-    </el-form>
-  </div>
+    <div>
+        <el-form
+            ref="formRef"
+            :label-position="labelPosition"
+            :model="innerFormData"
+            :label-width="labelWidth"
+        >
+            <el-form-item
+                v-for="item in items"
+                v-if="item.visiable === undefined ? true : item.visiable"
+                :label="item.label"
+                :key="item.value"
+                :rules="item.rules || [{ required: item.required, message: item.message }]"
+            >
+                <!-- 数字输入框 -->
+                <template v-if="item.type === number">
+                    <el-input-number
+                        v-model.number="innerFormData[item.value]"
+                        controls-position="right"
+                        :min="item.min"
+                        :max="item.max"
+                        :readonly="item.readonly"
+                        :placeholder="item.placeholder"
+                        @change="handleChange(item)"
+                    ></el-input-number>
+                </template>
+                <!-- 输入框 -->
+                <template v-if="item.type === 'input' ">
+                    <el-input
+                        v-model="innerFormData[item.value]"
+                        :type="itme.tag"
+                        :placeholder="item.placeholder"
+                        :readonly="item.readonly"
+                        :row="item.row"
+                        :autosize="item.autosize"
+                        :maxlength="item.maxlength"
+                        :show-word-limit="item.maxlength"
+                        :show-password="item.tag === 'password'"
+                        @change="handleChange(item)"
+                    >
+                    </el-input>
+                </template>
+                <!-- 下拉选择框 -->
+                <template v-if="item.type === 'select'">
+                    <el-select
+                        v-model="innerFormData[item.value]"
+                        :placeholder="item.plceholder"
+                        :disabled="item.disabled"
+                        :multiple="item.multiple"
+                    >
+                        <el-option
+                            v-for="option in item.options"
+                            :label="item.label"
+                            :value="item.value"
+                        >
+                            <slot scope="scope"></slot>
+                        </el-option>
+                    </el-select>
+                </template>
+
+                <template v-if="item.slotName">
+                    <slot
+                        :name="item.slotName"
+                        :item="item"
+                        :innerFormData="innerFormData"
+                    ></slot>
+                </template>
+            </el-form-item>
+
+            <el-form-item>
+                <el-button @click="handleSubmit" type="primary">保存</el-button>
+                <el-button @click="resetForm">重置</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
 </template>
 
 <script>
 export default {
-  props: {},
-  data() {
-    return {
-      innerFormData: {}
-    }
-  },
-  methods: {
-    /**
-     * change事件
-     */
-    handleChange(item) {
-      this.$emit(item, this.innerFormData)
+    props: {
+        config: {
+            type: Array,
+            default: () => []
+        },
+        data: {
+            type: Object,
+            default: () => {}
+        }
     },
-    /**
-     * 重置表单
-     */
-    resetForm() {
-      this.$refs.formRef.resetField()
-      this.$refs.formRef.clearValidate()
+    data() {
+        return {
+            innerFormData: {}
+        }
+    },
+    created() {
+         this.init()
+    },
+    watch: {
+        config() {
+            this.init()
+        }
+    },
+    methods: {
+        init() {
+            this.innerFormData = {}
+            this.config.forEach((item) => {
+                // 可以根据不同情况进行赋值， 比如日期就赋值为数组
+                this.$set(this.innerFormData, item.value, '')
+            })
+        },
+        /**
+         * change事件
+         */
+        handleChange(item) {
+            this.$emit(item, this.innerFormData)
+        },
+        /**
+         * 重置表单
+         */
+        resetForm() {
+            this.$refs.formRef.resetField()
+            this.$refs.formRef.clearValidate()
+        },
+        /**
+         * 提交表单
+         */
+        handleSubmit() {
+            this.$emit('submit', this.innerFormData)
+        }
     }
-  }
 }
 </script>
